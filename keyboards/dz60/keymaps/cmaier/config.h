0b46e28c69edd5ca1f51ff4f16c397e3be1354ec
Christian Maier <cmaierrr@gmail.com>
2019-09-01 15:57:57 +0200

    Turn off backlight when computer goes to sleep

--- keyboards/dz60/keymaps/cmaier/config.h
+++ keyboards/dz60/keymaps/cmaier/config.h
@@ -6,4 +6,9 @@
 // Fix KC_GESC conflict with Cmd+Alt+Esc on macos
 #define GRAVE_ESC_GUI_OVERRIDE
 
+// Turn off RGB when computer goes to sleep
+#ifdef RGBLIGHT_ENABLE
+#define RGBLIGHT_SLEEP
+#endif
+
 #endif
