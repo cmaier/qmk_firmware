09c6bab5ffff68121f7bb242f76a0026e545e6a5
Christian Maier <cmaierrr@gmail.com>
2020-05-21 00:46:43 +0200

    Remove RGB LED and Mouse support

--- keyboards/bioi/g60ble/rules.mk
+++ keyboards/bioi/g60ble/rules.mk
@@ -16,14 +16,14 @@ BOOTLOADER = qmk-dfu
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = lite    # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes      # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes      # Audio control and System control(+450)
 # CONSOLE_ENABLE = yes     # Console for debug(+400)
 COMMAND_ENABLE = yes       # Commands for debug and configuration
 # SLEEP_LED_ENABLE = yes   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes          # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes     # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no
 LTO_ENABLE = yes
 
 LAYOUTS = 60_ansi 60_iso 60_hhkb 60_ansi_split_bs_rshift 60_tsangan_hhkb
